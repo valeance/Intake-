568bc2bc2091e79960ce1629dccb7a66be9fe412
valeance <43921778+valeance@users.noreply.github.com>
2018-12-15 19:20:09 -0800

    Update intake.cpp

--- src/cpp/intake.cpp
+++ src/cpp/intake.cpp
@@ -8,3 +8,23 @@ void Intake::run_intake (float f_max_speed){
 std::cout<<"this is the amp reading: "<<amp<<std::endl;
 std::cout<<"this is the voltage reading"<<intake_talon->GetMotorOutputVoltage()<<std::endl;
 }
+void Intake::intake_options(float f_max_speed){//be sure that the input is a integar)
+Joystick *joystick;
+std::cout<<"this is the amp reading"<<intake_talon->GetOutputCurrent()<<std::endl;
+std::cout<<"this is the amp reading" << intake_talon->GetMotorOutputVoltage()<<std::endl;
+joystick= new Joystick(3);
+float hope_this_works= joystick->GetRawAxis(3);
+double x_pos =	hope_this_works;
+float adjusted_speed = x_pos*f_max_speed;
+intake_talon->Set(ControlMode::PercentOutput,adjusted_speed);
+}
+void Intake::options_3(int f_max_speed){
+Joystick *joystick;
+std::cout<<"this is the amp reading"<<intake_talon->GetOutputCurrent()<<std::endl;
+std::cout<<"this is the amp reading" << intake_talon->GetMotorOutputVoltage()<<std::endl;
+joystick= new Joystick(3);
+float hope_this_works= joystick->GetRawAxis(3);
+double x_pos =	hope_this_works;
+float adjusted_speed = x_pos/f_max_speed;
+intake_talon->Set(ControlMode::PercentOutput,adjusted_speed);
+}
